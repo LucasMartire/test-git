a68470e1c8881009d9d5513d66ba4e78fbb8e2a2
LucasMartire <martirelucas@itis-molinari.eu>
2024-01-07 19:43:57 +0100

    1

---
+++ index.js
@@ -0,0 +1,15 @@
+const tabella = document.getElementById("tabella");
+const bottone = document.getElementById("bottone");
+
+
+const template = `
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        
+    </tr>
+`
